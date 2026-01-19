322f77ac158b4f46a1c089ffb827b7b5643a163c
krishnalitaliya07-ops <jsheta15@gmail.com>
2026-01-19 09:02:32 +0530

    backend url added

--- client/src/Services/apis.js
+++ client/src/Services/apis.js
@@ -1,4 +1,4 @@
-const BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://grams-lyart.vercel.app/api"
+const BASE_URL =  "https://grams-lyart.vercel.app/api"
 
 // AUTH ENDPOINTS
 export const authEndpoints = {
